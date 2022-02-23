1451ebfe78364e6965e95795bb0aa3896ce900b0
lromanowicz <dummy@email.com>
2022-02-23 19:04:03 +0000

    change url

--- src/test/java/pl/pandait/panda/PandaApplicationSeleniumTest.java
+++ src/test/java/pl/pandait/panda/PandaApplicationSeleniumTest.java
@@ -32,7 +32,7 @@ public class PandaApplicationSeleniumTest {
         driver = new RemoteWebDriver(new URL("http://192.168.44.44:4444/wd/hub"), capabilities);
         // Pamiętaj, że aplikacja Spring musi działać! To znaczy też musi być włączona.
         
-        driver.get(String.format("slave:%s/", port));
+        driver.get(String.format("ubuntuslave:%s/", port));
 
 
         //Czekamy 2 sekundy
